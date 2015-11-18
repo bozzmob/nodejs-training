e5f88e7571cab142472030d2e493b9ceea8748cb
satjagan <satvik.jagannath7@gmail.com>
2015-11-18 12:36:03 +0530

    Node Modules

--- Day-1/Calculator/calculator.js
+++ Day-1/Calculator/calculator.js
@@ -23,11 +23,6 @@ var calculator= {
 	divide: function (x,y) {
 	return x/y;
 	}
-}
+};
 
-
-var x=100,y=50;
-console.log("Addition "+calculator.add(x,y));
-console.log("Subtraction "+calculator.subtract(x,y));
-console.log("Multiplication "+calculator.multiply(x,y));
-console.log("Division "+calculator.divide(x,y));
+module.exports = calculator;
---
+++ Day-1/Calculator/calculatorClient.js
@@ -0,0 +1,7 @@
+var calculator = require('./calculator');
+
+var x=100,y=50;
+console.log("Addition "+calculator.add(x,y));
+console.log("Subtraction "+calculator.subtract(x,y));
+console.log("Multiplication "+calculator.multiply(x,y));
+console.log("Division "+calculator.divide(x,y));
