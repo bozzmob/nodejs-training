ef5effb0028de8df41cde884b80eee3f9c7f4b41
satjagan <satvik.jagannath7@gmail.com>
2015-11-19 13:06:47 +0530

    New Task + Routes

--- Day-2/Task-Manager/app.js
+++ Day-2/Task-Manager/app.js
@@ -18,8 +18,20 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+app.use(function(req, res, next){
+  console.log("Before body parser - req.body = ", req.body);
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next){
+  console.log("Before body parser - req.body = ", req.body);
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- Day-2/Task-Manager/routes/taskRoutes.js
+++ Day-2/Task-Manager/routes/taskRoutes.js
@@ -3,12 +3,36 @@ var router = express.Router();
 
 var taskStore = [
 	{id:1, name: 'Fix the bug', isCompleted:false},
-	{id:2, name: 'Master JS', isCompleted:false}
+	{id:2, name: 'Master JS', isCompleted:false},
+	{id:3, name: 'Watch movie', isCompleted:true},
 ]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('All the tasks will be displayed here');
   res.render('tasks/index',{title:'Task Manager', list: taskStore});
+});
+
+router.get('/new', function(req, res, next) {
+  //res.send('All the tasks will be displayed here');
+  res.render('tasks/new',{title:'New Task'});
+});
+
+router.post('/new', function(req, res, next) {
+  //res.send('All the tasks will be displayed here');
+  var taskName = req.body.txtTask,
+  	  taskId = taskStore.reduce(function(result, task){
+  	  		return result > task.id ? result : task.id;
+  	  },0)+1;
+
+  var newTask = {
+  	id : taskId,
+  	name : taskName,
+  	isCompleted : false
+  };
+
+  taskStore.push(newTask);
+  res.redirect('/tasks');
+
 });
 
 module.exports = router;
