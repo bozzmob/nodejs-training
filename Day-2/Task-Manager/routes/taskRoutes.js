cbe257fef11637ea0c9f59b3c8209224a2046225
satjagan <satvik.jagannath7@gmail.com>
2015-11-19 15:16:54 +0530

    Task manager + App-Server update

--- Day-2/Task-Manager/routes/taskRoutes.js
+++ Day-2/Task-Manager/routes/taskRoutes.js
@@ -2,46 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 var taskStore = [
-	{id:1, name: 'Fix the bug', isCompleted:false},
-	{id:2, name: 'Master JS', isCompleted:false},
-	{id:3, name: 'Watch movie', isCompleted:true},
-]
+    {id : 1, name : 'Fix the bug', isCompleted : false},
+    {id : 2, name : 'Master JavaScript', isCompleted : false},
+    {id : 3, name : 'Watch Bahubali', isCompleted : true}
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('All the tasks will be displayed here');
-  res.render('tasks/index',{title:'Task Manager', list: taskStore});
+  res.render('tasks/index', {title : 'Task Organizer', list : taskStore});
 });
 
-router.get('/new', function(req, res, next) {
-  //res.send('All the tasks will be displayed here');
-  res.render('tasks/new',{title:'New Task'});
+router.get('/new', function(req, res, next){
+    res.render('tasks/new', {title : 'New Task'});
 });
 
-router.post('/new', function(req, res, next) {
-  //res.send('All the tasks will be displayed here');
-  var taskName = req.body.txtTask,
-  	  taskId = taskStore.reduce(function(result, task){
-  	  		return result > task.id ? result : task.id;
-  	  },0)+1;
-
-  var newTask = {
-  	id : taskId,
-  	name : taskName,
-  	isCompleted : false
-  };
-  taskStore.push(newTask);
-  res.redirect('/tasks');
+router.post('/new', function(req, res, next){
+    var taskName = req.body.txtTask,
+        taskId = taskStore.reduce(function(result, task){
+            return result > task.id ? result : task.id;
+        },0)+1;
+    var newTask = {
+        id : taskId,
+        name : taskName,
+        isCompleted : false
+    };
+    taskStore.push(newTask);
+    res.redirect('/tasks');
 });
 
-router.get('/toggle/:id', function(req, res, next) {
-  //res.send('All the tasks will be displayed here');
-  var taskId = parseInt(req.params.id, 10);
-  var task = taskStore.filter(function(task){
-  	return task.id === taskId;
-  })[0];
-  if(task)
-  	task.isCompleted = !task.isCompleted;
-  res.redirect('/tasks');
+router.get('/toggle/:id', function(req, res, next){
+    var taskId = parseInt(req.params.id, 10);
+    var task = taskStore.filter(function(task){
+        return task.id === taskId;
+    })[0];
+    if (task)
+        task.isCompleted = !task.isCompleted;
+    res.redirect('/tasks');
 });
 
 module.exports = router;
--- Day-2/Task-Manager/routes/users.js
+++ Day-2/Task-Manager/routes/users.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/new', function(req, res, next) {
-  res.send('New user will be added here');
+router.get('/new', function(req, res, next){
+    res.send('New user will be added here');
 });
 
 module.exports = router;
